dfd2aa2cb2d29fd14a34cf591ffb6fa3c89f8ea6
IfCoinOfficial <rlaxorjs88@naver.com>
2025-06-19 12:44:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ console.log("🔐 BOT_TOKEN 확인:", process.env.BOT_TOKEN);
 const token = process.env.BOT_TOKEN || "여기에_직접_토큰_입력_금지_⚠️";
 const bot = new TelegramBot(token, { polling: true });
 
+const mainKeyboard = {
+  reply_markup: {
+    keyboard: [["/if", "/event"]],
+    resize_keyboard: true,
+    one_time_keyboard: false,
+  },
+};
+
 bot.on("new_chat_members", (msg) => {
   const chatId = msg.chat.id;
   const welcome = "🎉 IF 프로젝트에 참여하신 것을 환영합니다!\n\n📘 장기 투자가 가능한 IF를 선택하여 또 다른 미래를 만들어보세요.\n/event를 입력하시고 1만IF도 받아가세요!";
